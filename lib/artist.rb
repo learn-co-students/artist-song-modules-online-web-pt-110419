5bf40a39a2b029eabb98cd91113f26b243b2cf8a
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 00:54:09 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,21 @@
 require 'pry'
 
+
 class Artist
+
+  extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
+    # shovels instances of artist into @@artist array
   end
 
   def self.find_by_name(name)
@@ -19,24 +26,27 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
     song.artist = self
+    # the song should know which artist it belongs to as soon as its added to the artist's song collection.
+    # so we tell it as such with song.artist = self
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
+    # binding.pry
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,49 @@
 require 'pry'
 
 class Song
+
+  extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
+    # returns the first object.name that matches to the argument
+    # binding.pry
   end
 
   def self.all
+    # returns all instances of Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  #   # removes all elements within @@songs and returns an empty array
+  # end
+  #
+  # def self.count
+  #   self.all.count
+  #   # counts the number of elements in an array
+  # end
 
   def artist=(artist)
     @artist = artist
+    # setter method for artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  #   # binding.pry
+  #   #downcase replaces all capital letters in name variable with lower case letters and gsub scans name variable string and replaces ' ' with '-'
+  # end
 end
